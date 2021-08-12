9aae8b9733656773b148ecf8af298685a5fe3b70
Warner <warnebre@amazon.com>
2021-08-12 10:16:37 -0400

    added trial db

--- application.py
+++ application.py
@@ -6,17 +6,25 @@ from flask import Flask,request
 mysql = MySQL()
 application = Flask(__name__)
 
+
 @application.route("/index")
 def index():
-    application.config['RDS_HOSTNAME'] = 'database-1.cn1wgl48ybn6.eu-west-2.rds.amazonaws.com'
-    application.config['RDS_PORT'] = '3306'
-    application.config['RDS_DB_NAME'] = 'EUcustdb'
-    application.config['RDS_USERNAME'] = 'admin'
-    application.config['RDS_PASSWORD'] = 'Hollywarner10'
+    #application.config['RDS_HOSTNAME'] = 'database-1.cn1wgl48ybn6.eu-west-2.rds.amazonaws.com'
+    #application.config['RDS_PORT'] = '3306'
+    #application.config['RDS_DB_NAME'] = 'EUcustdb'
+    #application.config['RDS_USERNAME'] = 'admin'
+    #application.config['RDS_PASSWORD'] = 'Hollywarner10'
+
+    application.config['MYSQL_DATABASE_USER'] = 'admin'
+    application.config['MYSQL_DATABASE_PASSWORD'] = 'Hollywarner10'
+    application.config['MYSQL_DATABASE_DB'] = 'EUcustdb'
+    application.config['MYSQL_DATABASE_HOST'] = 'database-1.cn1wgl48ybn6.eu-west-2.rds.amazonaws.com'
+    application.config['MYSQL_DATABASE_ROOT'] = '3306'
 
     mysql.init_app(application)
 
     cursor = mysql.connect().cursor()
+    #cursor.execute('SELECT * FROM eu_cust_data')
     cursor.execute('SELECT * FROM EU_cust_data')
     data = cursor.fetchall()
     return render_template('index.html', output_data=data)
@@ -24,15 +32,16 @@ def index():
 @application.route("/Authenticate")
 def Authenticate():
 
-    #app.config['MYSQL_DATABASE_USER'] = 'root'
-    #app.config['MYSQL_DATABASE_PASSWORD'] = 'Hollywarner10'
-    #app.config['MYSQL_DATABASE_DB'] = 'inventorydb'
-    #app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-    application.config['RDS_HOSTNAME'] = 'globalauroradb-cluster-1.cluster-ro-cn1wgl48ybn6.eu-west-2.rds.amazonaws.com'
-    application.config['RDS_PORT'] = '3306'
-    application.config['RDS_DB_NAME'] = 'inventorydb'
-    application.config['RDS_USERNAME'] = 'admin'
-    application.config['RDS_PASSWORD'] = 'Hollywarner10'
+    application.config['MYSQL_DATABASE_USER'] = 'root'
+    application.config['MYSQL_DATABASE_PASSWORD'] = 'Hollywarner10'
+    application.config['MYSQL_DATABASE_DB'] = 'inventorydb'
+    application.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+    #application.config['RDS_HOSTNAME'] = 'globalauroradb-cluster-1.cluster-ro-cn1wgl48ybn6.eu-west-2.rds.amazonaws.com'
+    #application.config['RDS_PORT'] = '3306'
+    #application.config['RDS_DB_NAME'] = 'inventorydb'
+    #application.config['RDS_USERNAME'] = 'admin'
+    #application.config['RDS_PASSWORD'] = 'Hollywarner10'
 
     mysql.init_app(application)
 
@@ -52,4 +61,5 @@ def hello_world():
     return render_template('button.html')
 
 if __name__ == '__main__':
-    application.run()
+    #application.run()
+    application.run(host='localhost', port=5000)
