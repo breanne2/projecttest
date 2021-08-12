e80e0c55dc456f69531eee2b4ba36ba44618ea46
Warner <warnebre@amazon.com>
2021-08-11 20:20:00 -0400

    small change

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'London page!'
 
 if __name__ == '__main__':
     application.run()
