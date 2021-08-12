18c61da118ed80987d02726706cf1c6f7d5c0522
Warner <warnebre@amazon.com>
2021-08-11 20:32:41 -0400

    added all db

--- application.py
+++ application.py
@@ -21,9 +21,35 @@ def index():
     data = cursor.fetchall()
     return render_template('index.html', output_data=data)
 
+@application.route("/Authenticate")
+def Authenticate():
+
+    #app.config['MYSQL_DATABASE_USER'] = 'root'
+    #app.config['MYSQL_DATABASE_PASSWORD'] = 'Hollywarner10'
+    #app.config['MYSQL_DATABASE_DB'] = 'inventorydb'
+    #app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+    application.config['RDS_HOSTNAME'] = 'globalauroradb-cluster-1.cluster-ro-cn1wgl48ybn6.eu-west-2.rds.amazonaws.com'
+    application.config['RDS_PORT'] = '3306'
+    application.config['RDS_DB_NAME'] = 'inventorydb'
+    application.config['RDS_USERNAME'] = 'admin'
+    application.config['RDS_PASSWORD'] = 'Hollywarner10'
+
+    mysql.init_app(application)
+
+    cursor = mysql.connect().cursor()
+    #Productname = request.args.get('productname')
+    cursor = mysql.connect().cursor()
+    cursor.execute('SELECT * FROM items')
+    data = cursor.fetchall()
+    if data is None:
+        return "Username or Password is wrong"
+    else:
+        return render_template('inventory.html', output_data=data)
+
 @application.route('/')
 def hello_world():
-    return 'London page!'
+    #return 'London page!'
+    return render_template('button.html')
 
 if __name__ == '__main__':
     application.run()
