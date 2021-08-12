f9bff5439be0cb4fe8b9e217ad2939f1df56b1b5
Warner <warnebre@amazon.com>
2021-08-11 20:27:36 -0400

    added db for europe

--- application.py
+++ application.py
@@ -3,8 +3,23 @@ from flask import Flask, render_template
 from flaskext.mysql import MySQL
 from flask import Flask,request
 
+mysql = MySQL()
 application = Flask(__name__)
 
+@application.route("/index")
+def index():
+    application.config['RDS_HOSTNAME'] = 'database-1.cn1wgl48ybn6.eu-west-2.rds.amazonaws.com'
+    application.config['RDS_PORT'] = '3306'
+    application.config['RDS_DB_NAME'] = 'EUcustdb'
+    application.config['RDS_USERNAME'] = 'admin'
+    application.config['RDS_PASSWORD'] = 'Hollywarner10'
+
+    mysql.init_app(application)
+
+    cursor = mysql.connect().cursor()
+    cursor.execute('SELECT * FROM EU_cust_data')
+    data = cursor.fetchall()
+    return render_template('index.html', output_data=data)
 
 @application.route('/')
 def hello_world():
